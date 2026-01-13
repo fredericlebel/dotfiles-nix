b61f728f1ece4bd0bbe4da5cdb5e9eb5ed8d7d30
Frédéric Lebel <flebel@opval.com>
2026-01-12 21:41:54 -0500

    feature: ajout de mac-app-util pour gérer la reconnaissance des apps installé par nix dans macos

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -3,39 +3,46 @@
   user,
   ...
 }: let
+  mac-app-util = inputs.mac-app-util;
   pkgs-unstable = inputs.nixpkgs;
   home-manager = inputs.home-manager;
   darwin = inputs.darwin;
   nix-homebrew = inputs.nix-homebrew;
-
-  # 1. AJOUT : On récupère l'input Disko
   disko = inputs.disko;
 in {
   mkDarwinSystem = hostName:
     darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       specialArgs = {inherit inputs user;};
+
       modules = [
+        mac-app-util.darwinModules.default
         ../hosts/${hostName}/default.nix
+
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.extraSpecialArgs = {inherit inputs user;};
-          home-manager.users.${user} = import ../hosts/${hostName}/home.nix;
+          home-manager.users.${user} = {
+            imports = [
+              ../hosts/${hostName}/home.nix
+              mac-app-util.homeManagerModules.default
+            ];
+          };
         }
+
         nix-homebrew.darwinModules.nix-homebrew
       ];
     };
-
   mkNixosSystem = hostName:
     pkgs-unstable.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs user;};
       modules = [
         disko.nixosModules.disko
-
         ../hosts/${hostName}/default.nix
+
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
