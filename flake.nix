1849633695c8a18b34ee2cd1c04d572e68908b16
Frédéric Lebel <flebel@opval.com>
2026-01-15 17:58:18 -0500

    feature: passage au repo officiel de nix-darwin et activation de tailscale

--- flake.nix
+++ flake.nix
@@ -4,18 +4,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    darwin.url = "github:LnL7/nix-darwin";
+    darwin.url = "github:nix-darwin/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nix-homebrew.inputs.nixpkgs.follows = "nixpkgs";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
     mac-app-util.url = "github:hraban/mac-app-util";
+    mac-app-util.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {
@@ -29,14 +31,7 @@
     systems = ["aarch64-darwin" "x86_64-linux"];
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    formatter = forAllSystems (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        pkgs.writeShellScriptBin "nixfmt" ''
-          ${pkgs.alejandra}/bin/alejandra .
-        ''
-    );
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
     packages."aarch64-darwin".default = self.darwinConfigurations."caladan".system;
     packages."x86_64-linux".default = self.nixosConfigurations."ix".system;
