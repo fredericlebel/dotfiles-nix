73e52b8343c438c292cf303f484d251e039f1339
Frédéric Lebel <flebel@opval.com>
2026-01-31 13:15:13 -0500

    feature: ajout des features de home dans le user et les hosts caladan et ix

--- hosts/caladan/home.nix
+++ hosts/caladan/home.nix
@@ -1,26 +1,10 @@
-{ user, ... }:
-{
+{user, ...}: {
   imports = [
     ../../users/${user}/base.nix
+    ../../modules/home/bundles/desktop
   ];
-  nixpkgs.config.allowUnfree = true;
-  my.features = {
-    bat.enable = true;
-    direnv.enable = true;
-    eza.enable = true;
-    fzf.enable = true;
-    git.enable = true;
-    gpg.enable = true;
-    htop.enable = true;
-    k9s.enable = true;
-    neovim.enable = true;
-    screen.enable = true;
-    starship.enable = true;
-    vscode.enable = true;
-    wezterm.enable = true;
-    yazi.enable = true;
-    zsh.enable = true;
-  };
+
+  my.bundles.desktop.enable = true;
 
   home = {
     username = user;
