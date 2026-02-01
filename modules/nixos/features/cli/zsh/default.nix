c1e5ef334eacd9805602d0bcb578ca73bb07d75e
Frédéric Lebel <flebel@opval.com>
2026-01-31 22:34:17 -0500

    feature: conversion du module zsh en feature cli.zsh

---
+++ modules/nixos/features/cli/zsh/default.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.my.features.cli.zsh;
+in {
+  options.my.features.cli.zsh = {
+    enable = lib.mkEnableOption "Zsh System Support";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableBashCompletion = true;
+    };
+
+    environment.shells = with pkgs; [zsh];
+
+    environment.systemPackages = with pkgs; [
+      zsh
+      zsh-syntax-highlighting
+      zsh-autosuggestions
+    ];
+
+    users.defaultUserShell = pkgs.zsh;
+  };
+}
