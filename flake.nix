9e37b9ccd69d06f74c67c60a2881a04ddfac589c
Frédéric Lebel <flebel@opval.com>
2026-02-08 10:48:07 -0500

    feature: séparation de la logique des données dans le flake.nix

--- flake.nix
+++ flake.nix
@@ -1,41 +1,33 @@
 {
-  description = "Config Nix Multi-Host de Frédéric Lebel";
+  description = "Infrastructure orchestrée de Frédéric Lebel";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     colmena = {
       url = "github:zhaofengli/colmena";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     darwin = {
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     mac-app-util = {
       url = "github:hraban/mac-app-util";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-
     nix-vscode-extensions = {
       url = "github:nix-community/nix-vscode-extensions";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -45,15 +37,18 @@
   outputs =
     inputs@{
       self,
-      colmena,
       nixpkgs,
+      colmena,
       ...
     }:
     let
       user = "flebel";
-
       inventory = import ./nix/lib/inventory.nix;
       mylib = import ./nix/lib/helpers.nix { inherit inputs user; };
+
+      nixosHosts = nixpkgs.lib.filterAttrs (n: v: !v.isDarwin) inventory;
+      darwinHosts = nixpkgs.lib.filterAttrs (n: v: v.isDarwin) inventory;
+      colmenaHosts = nixpkgs.lib.filterAttrs (n: v: v.deployment != null) inventory;
 
       systems = [
         "aarch64-darwin"
@@ -64,62 +59,71 @@
     {
       formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
 
-      darwinConfigurations = {
-        "caladan" = mylib.mkSystem {
-          hostName = "caladan";
-          inherit (inventory.caladan) system isDarwin;
-        };
-      };
+      darwinConfigurations = nixpkgs.lib.mapAttrs (
+        name: conf:
+        mylib.mkSystem {
+          hostName = name;
+          inherit (conf) system isDarwin;
+        }
+      ) darwinHosts;
 
-      nixosConfigurations = {
-        "ix" = mylib.mkSystem {
-          hostName = "ix";
-          inherit (inventory.ix) system isDarwin;
-        };
-      };
+      nixosConfigurations = nixpkgs.lib.mapAttrs (
+        name: conf:
+        mylib.mkSystem {
+          hostName = name;
+          inherit (conf) system isDarwin;
+        }
+      ) nixosHosts;
 
       colmena = {
         meta = {
           nixpkgs = import nixpkgs { system = "x86_64-linux"; };
           specialArgs = { inherit inputs user; };
         };
-
-        "ix" =
-          { ... }:
-          let
-            hostMeta = import ./hosts/ix/host-meta.nix;
-          in
-          {
-            deployment = inventory.ix.deployment // {
-              buildOnTarget = true;
-              targetUser = user;
-            };
-
-            _module.args.myMeta = mylib.defaultMeta // hostMeta;
-
-            imports = [
-              ./hosts/ix/configuration.nix
-              inputs.sops-nix.nixosModules.sops
-              inputs.disko.nixosModules.disko
-              inputs.home-manager.nixosModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = false;
-                  useUserPackages = true;
-                  backupFileExtension = "hm-backup";
-                  extraSpecialArgs = { inherit inputs user; };
-                  users.${user} = import ./hosts/ix/home.nix;
-                };
-              }
-            ];
+      }
+      // (nixpkgs.lib.mapAttrs (
+        name: conf:
+        { ... }:
+        {
+          deployment = conf.deployment // {
+            buildOnTarget = true;
+            targetUser = user;
           };
-      };
+          _module.args.myMeta = mylib.defaultMeta // (import ./hosts/${name}/host-meta.nix);
+
+          imports = [
+            ./hosts/${name}/configuration.nix
+            inputs.sops-nix.nixosModules.sops
+            inputs.disko.nixosModules.disko
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = false;
+                useUserPackages = true;
+                extraSpecialArgs = { inherit inputs user; };
+                users.${user} = import ./hosts/${name}/home.nix;
+              };
+            }
+          ];
+        }
+      ) colmenaHosts);
 
       colmenaHive = colmena.lib.makeHive self.outputs.colmena;
 
-      packages = {
-        "aarch64-darwin".default = self.darwinConfigurations."caladan".system;
-        "x86_64-linux".default = self.nixosConfigurations."ix".config.system.build.toplevel;
-      };
+      packages = forAllSystems (
+        system:
+        let
+          matchingHosts = nixpkgs.lib.filterAttrs (n: v: v.system == system) inventory;
+        in
+        nixpkgs.lib.mapAttrs' (
+          name: conf:
+          nixpkgs.lib.nameValuePair name (
+            if conf.isDarwin then
+              self.darwinConfigurations.${name}.system
+            else
+              self.nixosConfigurations.${name}.config.system.build.toplevel
+          )
+        ) matchingHosts
+      );
     };
 }
