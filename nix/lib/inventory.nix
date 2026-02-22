d70b4fa313b86bfccb92477d3117194600f7ec40
Frédéric Lebel <flebel@opval.com>
2026-02-22 14:42:19 -0500

    feature: ajout du host ecaz

--- nix/lib/inventory.nix
+++ nix/lib/inventory.nix
@@ -1,4 +1,16 @@
 {
+  ecaz = {
+    system = "x86_64-linux";
+    isDarwin = false;
+    deployment = {
+      targetHost = "ecaz.opval.com";
+      tags = [
+        "vps"
+        "cloud"
+      ];
+    };
+  };
+
   ix = {
     system = "x86_64-linux";
     isDarwin = false;
