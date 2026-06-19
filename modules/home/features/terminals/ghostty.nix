ba42dec304fecd10a5213a8f24de3c73efdce2f5
Frédéric Lebel <flebel@opval.com>
2026-06-19 14:39:52 -0400

    fix(ghostty): use standard zsh command instead of zsh.nix

--- modules/home/features/terminals/ghostty.nix
+++ modules/home/features/terminals/ghostty.nix
@@ -23,7 +23,7 @@ in
         window-padding-x = 10;
         window-padding-y = 10;
         macos-titlebar-proxy-icon = "hidden";
-        command = "${pkgs.zsh}/bin/zsh.nix";
+        command = "${pkgs.zsh}/bin/zsh";
         focus-follows-mouse = true;
         mouse-hide-while-typing = true;
         confirm-close-surface = false;
