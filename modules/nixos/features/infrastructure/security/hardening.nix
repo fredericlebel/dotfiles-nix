b4ad857d99e945b8ae81162e14ce7cbe92abe91a
Frédéric Lebel <flebel@opval.com>
2026-08-03 22:04:14 -0400

    feat(security): durcissement avance du noyau Linux via sysctl (kernel, fs, net anti-spoofing)

--- modules/nixos/features/infrastructure/security/hardening.nix
+++ modules/nixos/features/infrastructure/security/hardening.nix
@@ -14,11 +14,44 @@ in
   config = lib.mkIf cfg.enable {
     boot = {
       kernel.sysctl = {
-        "kernel.dmesg_restrict" = 1;
-        "net.ipv4.conf.all.log_martians" = 1;
-        "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
+        # --- Kernel Protections ---
         "kernel.kptr_restrict" = 2;
+        "kernel.dmesg_restrict" = 1;
         "kernel.unprivileged_bpf_disabled" = 1;
+        "kernel.sysrq" = 0;
+        "kernel.yama.ptrace_scope" = 1;
+
+        # --- Filesystem Protections ---
+        "fs.protected_hardlinks" = 1;
+        "fs.protected_symlinks" = 1;
+        "fs.protected_fifos" = 2;
+        "fs.protected_regular" = 2;
+        "fs.suid_dumpable" = 0;
+
+        # --- Network Stack Hardening ---
+        "net.ipv4.conf.all.log_martians" = 1;
+        "net.ipv4.conf.default.log_martians" = 1;
+        "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
+        "net.ipv4.tcp_syncookies" = 1;
+        "net.ipv4.tcp_rfc1337" = 1;
+
+        # Disable ICMP Redirect Acceptance (Anti-Spoofing / Anti-MITM)
+        "net.ipv4.conf.all.accept_redirects" = 0;
+        "net.ipv4.conf.default.accept_redirects" = 0;
+        "net.ipv4.conf.all.secure_redirects" = 0;
+        "net.ipv4.conf.default.secure_redirects" = 0;
+        "net.ipv6.conf.all.accept_redirects" = 0;
+        "net.ipv6.conf.default.accept_redirects" = 0;
+
+        # Disable ICMP Redirect Sending
+        "net.ipv4.conf.all.send_redirects" = 0;
+        "net.ipv4.conf.default.send_redirects" = 0;
+
+        # Disable IP Source Routing
+        "net.ipv4.conf.all.accept_source_route" = 0;
+        "net.ipv4.conf.default.accept_source_route" = 0;
+        "net.ipv6.conf.all.accept_source_route" = 0;
+        "net.ipv6.conf.default.accept_source_route" = 0;
       };
 
       tmp = {
