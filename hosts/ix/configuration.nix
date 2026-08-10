89861c7cccb30e30bff794065930ec1750ed38c5
Frédéric Lebel <flebel@opval.com>
2026-08-10 18:26:03 -0400

    refactor(hosts): suppression des imports manuels de features dans ecaz et ix

--- hosts/ix/configuration.nix
+++ hosts/ix/configuration.nix
@@ -11,13 +11,6 @@
     ../../users/${user}/system.nix
 
     ../../modules/nixos/bundles/vps-base.nix
-    ../../modules/nixos/features/infrastructure/caddy.nix
-    ../../modules/nixos/features/infrastructure/home-assistant.nix
-    ../../modules/nixos/features/infrastructure/observability
-    ../../modules/nixos/features/infrastructure/security/openssh.nix
-    ../../modules/nixos/features/infrastructure/security/suricata.nix
-    ../../modules/nixos/features/infrastructure/tailscale.nix
-    ../../modules/nixos/features/infrastructure/vaultwarden.nix
   ];
 
   # On fusionne la Spec unifiée avec la config technique locale
