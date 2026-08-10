aee74ddeccec059e3211651b0032243fd7e6b2b4
Frédéric Lebel <flebel@opval.com>
2026-08-10 18:18:29 -0400

    refactor(hosts): factorisation des hôtes ecaz et ix via vps-base

--- hosts/ecaz/configuration.nix
+++ hosts/ecaz/configuration.nix
@@ -1,18 +1,16 @@
 {
   config,
-  modulesPath,
   user,
   lib,
   ...
 }:
 {
   imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
     ./disko.nix
 
     ../../users/${user}/system.nix
 
-    ../../modules/nixos/bundles/base-server.nix
+    ../../modules/nixos/bundles/vps-base.nix
     ../../modules/nixos/features/infrastructure/security/openssh.nix
     ../../modules/nixos/features/infrastructure/tailscale.nix
     ../../modules/nixos/features/infrastructure/postgresql.nix
@@ -27,51 +25,13 @@
     };
   };
 
-  # Activation manuelle du bundle (importé via imports)
-  my.bundles.base-server.enable = true;
+  # Activation du bundle VPS de base
+  my.bundles.vps-base.enable = true;
 
-  boot.loader.grub = {
-    enable = true;
-    efiSupport = true;
-    efiInstallAsRemovable = true;
+  networking.hostName = "ecaz";
+
+  sops.secrets.postgres-opentofu-password = {
+    owner = "postgres";
+    mode = "0400";
   };
-
-  users.users.${user} = {
-    extraGroups = [
-      "wheel"
-    ];
-    home = "/home/${user}";
-  };
-
-  networking = {
-    hostName = "ecaz";
-    domain = "opval.com";
-
-    firewall = {
-      enable = true;
-    };
-    nftables.enable = true;
-  };
-
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 2048;
-    }
-  ];
-
-  sops.secrets = {
-    tailscale-key = {
-      owner = "root";
-      mode = "0400";
-    };
-    postgres-opentofu-password = {
-      owner = "postgres";
-      mode = "0400";
-    };
-  };
-
-  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-
-  system.stateVersion = "26.05";
 }
