427dc1c0d4716951b3b2c284ecd19312e760833f
Frédéric Lebel <flebel@opval.com>
2026-08-10 18:33:09 -0400

    feat(nix/lib): création du registre d'inventaire d'hôtes inventory.nix

---
+++ nix/lib/inventory.nix
@@ -0,0 +1,6 @@
+# Inventaire global des hôtes (Single Source of Truth)
+{
+  ix = import ../../hosts/ix/spec.nix;
+  ecaz = import ../../hosts/ecaz/spec.nix;
+  caladan = import ../../hosts/caladan/spec.nix;
+}
