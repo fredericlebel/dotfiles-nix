db79c589488dfe247dafeaf4b1eb56735a21c96e
Frédéric Lebel <flebel@opval.com>
2026-08-10 18:37:03 -0400

    refactor(hosts): déplacement de l'inventaire vers hosts/default.nix (DDD)

---
+++ hosts/default.nix
@@ -0,0 +1,6 @@
+# Inventaire centralisé du domaine des hôtes (Single Source of Truth / DDD)
+{
+  ix = import ./ix/spec.nix;
+  ecaz = import ./ecaz/spec.nix;
+  caladan = import ./caladan/spec.nix;
+}
--- nix/lib/inventory.nix
+++
@@ -1,6 +0,0 @@
-# Inventaire global des hôtes (Single Source of Truth)
-{
-  ix = import ../../hosts/ix/spec.nix;
-  ecaz = import ../../hosts/ecaz/spec.nix;
-  caladan = import ../../hosts/caladan/spec.nix;
-}
