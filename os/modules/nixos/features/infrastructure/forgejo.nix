8f1820f09b0e1a94dcb920cb19f2d6c55292e8c5
Frédéric Lebel <flebel@opval.com>
2026-08-27 21:42:16 -0400

    fix(os/modules/forgejo): utilisation du mode bridge et mapping de ports au lieu du mode host

--- os/modules/nixos/features/infrastructure/forgejo.nix
+++ os/modules/nixos/features/infrastructure/forgejo.nix
@@ -63,16 +63,18 @@ in
       backend = "podman";
       containers.forgejo = {
         image = "codeberg.org/forgejo/forgejo:15";
-        extraOptions = [ "--network=host" ];
+        ports = [
+          "127.0.0.1:3000:3000"
+          "${toString cfg.sshPort}:22"
+        ];
         environment = {
           USER_UID = "1000";
           USER_GID = "1000";
           FORGEJO__database__DB_TYPE = "postgres";
-          FORGEJO__database__HOST = "127.0.0.1:5432";
+          FORGEJO__database__HOST = "${config.networking.hostName}.${myMeta.connectivity.tailnet}:5432";
           FORGEJO__database__NAME = "forgejo";
           FORGEJO__database__USER = "forgejo";
           FORGEJO__server__SSH_PORT = toString cfg.sshPort;
-          FORGEJO__server__SSH_LISTEN_PORT = toString cfg.sshPort;
           FORGEJO__server__SSH_DOMAIN = "${config.networking.hostName}.${myMeta.connectivity.tailnet}";
         };
         environmentFiles = lib.mkIf (cfg.envFile != null) [ cfg.envFile ];
